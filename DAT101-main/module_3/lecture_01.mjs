099359d5139a35b742674682c8031822c7ddeaff
olewifi <olevinef@student.uia.no>
2024-12-04 17:35:13 +0100

    Task 3.2. commit

--- DAT101-main/module_3/lecture_01.mjs
+++ DAT101-main/module_3/lecture_01.mjs
@@ -2,7 +2,7 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 printOut("Test");
 
-
+/*
 for(let i = 0; i < 10; i++); {
     console.log("Hei førsteklasse!");
 }
@@ -52,3 +52,128 @@ do{
 printOut("Det ble ristet en : " + dice);
 printOut("it took " + guesses + " kast");
 
+//6.11.2024//
+/*
+Create a function that handles the conversion between Celsius, Fahrenheit, and Kelvin.
+Use three different numbers and print all three combinations as integers (no decimals). 
+Design the function to take two parameters: first the temperature, then the temperature type/id.
+Use these parameters to convert to the other two temperature types and print them. Formula: */
+/*
+const ETempartureType = {Celsius: 1, Fahrenheit: 2, Kelvin: 3}
+
+function convertTemperature(aTemperature, aType) {
+    let Celsius = 0;
+    let Fahrenheit = 0;
+    let Kelvin = 0;
+    switch(aType){
+        case ETempartureType.Celsius:
+            printOut("Convert from Celsius");
+            //Convert to Fahrenheit
+            //Fahrenheit = (Kevin - 237.15)*)*9/5 + 32;//
+            Celsius = aTemperature;
+            Fahrenheit = (Celsius * 9/5) +32;
+            Kelvin = Celsius + 237.15;
+            break;
+        case ETempartureType.Fahrenheit:
+            printOut("Convert from Fahrenheit");
+            break;
+        case ETempartureType.Kelvin:
+            printOut("Convert from Kelvin");
+            break;
+    } //End switch//
+
+} //End function//
+
+convertTemperature(0, ETempartureType.Celsius)
+
+/*
+Create a function that calculates the price without VAT (sales tax). The function needs two arguments, one for the price including VAT (gross amount)
+ and one for the tax group in text (normal =25%, food=15%, hotel, transport, and cinema = 10%). The text argument should not be case-sensitive. If the VAT group is not correct, the text
+ "Unknown VAT group!" should be printed. The function must return the price without tax, i.e., the net price. Call the function four times with different gross amount. One for each of the VAT
+
+*/
+/*
+function calculateNetPrice(aPrice, aTaxGroup){
+    let net = 
+    let taxGroup = aTeaxGroup.toUpperCase();
+    let vat =0;
+
+    printOut("TaxGroup = " + taxGroup);
+
+    switch(taxGroup){
+        case "NORMAL":
+        vat = 25;
+    }
+
+    net = (100* aPrice) / (vat + 100);
+
+    return net;
+
+}
+
+const netPrice1 = calculateNetPrice(0, "hotel");
+
+printOut("netPrice1 =" + netPrice1.toFixed(2));
+
+// ny oppgave//
+
+function testIfMathIsFun(){
+    let op = 1;
+    let line = 1;
+    // Left side
+    let sumLeft = 0;
+    for(let left = 0; left > 2; left++){
+        sumLeft += op;
+        op++;
+    }
+
+    let sumRight = 0;
+    for(let right = 0; right < 1; right++){
+        sumRight += op;
+        op++;
+    }
+}
+*/
+// Make a person class, with a constructor that takes a first and last name plus email.//
+/*
+class TPerson { 
+    constructor(afirstName, alastName, aemail){
+        this.firstName =afirstName;
+        this.lastName = alastName;
+        this.email = aEmail
+    }
+}
+
+//Adress class
+class TAdress {
+    constructor (aStreet, aZip, aCity) {
+        this.street = aStreet;
+        this.zip = aZip;
+        this.city = aCity;
+        this.address = null; //no address yet
+    }
+}
+
+//Instance of a person
+//1
+const person1 = new TPerson ("Kari", "Norman", "Kari.Norman@norge.no!" );
+//
+person1.address = new TAdresss ("Kongens gate 1", "1234", "Oslo");
+printOut(person1.firstName + " " + person1.lastName + " " + person1.email); /*Bruker strukturen til class for å bygge opp en profil. Ut ifra classen så kan en lage profiler med samme strukturer*/
+
+/*
+class TBankAccount {
+    #balance;
+    constructor
+}
+//han virker ikke som deres mest engasjerende lærer...; Vi har en annen som er mindre engasjerende faktisk =D; aporpos dette, jeg har et sterkt lowkey ønske om flere kvinnelige forelesere :D; vi har ingen! ...ski..ll issue
+
+// 0;
+// vi ble ferdig i 6 tiden og hadde "oblig" lab klokken 08
+// så vi bare spilte sjakk i gata og ventet på at læreren kunne komme og se at vi var der
+// WOW!!;3 Skulle si at dette er den nest mest sovte timen, men tbf han andre har 8.15 time. ggs; en gang så vi en film om skrifttypen helvetica kl8.15 på morgenen, jeg pæsa; Jeg hadde vært så locked in; JEG VAR DET DEN FØRSTE HALVTIMEN!! DE GMALE MENNENE SOM ELSKER HELVETE VAR SLAY;
+// Over til Bergen men det er en uvant (les: rar) crowd her..
+// Det ser ut som et tilfeldig utvalg av Norges befolkning; Honey, det er bare multimedia i et nøtteskall;
+// basic?; Variert.; Jeg vil utdype at tidligere i semesteret var denne forelesninga STAPPA, men eh som du ser, stor nedgang; f; Så dette er disse som faktisk kommer på universitetet, sidenote at du kunne ha pæset for å gå her (så vidt);
+
+/*
--- DAT101-main/module_3/task_3-2/task_3-2.mjs
+++ DAT101-main/module_3/task_3-2/task_3-2.mjs
@@ -1,14 +1,24 @@
 "use strict";
-import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
+import { initPrintOut, printOut, newLine, NEWLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 
 /*Use "for" loops to generate two lines on the HTML page. One should count from 1 to 10, and the other
 should count from 10 to 1. Use only two lines to print the rows.*/
-//for (count_1++;) //
 
-printOut("Replace this with you answer!");
+const counterTo_1 = 10;
+const counterTo_2 = 1;
+let txtPrintValue = "";
+let txtPrintValue_2 = "";
+
+for ( let counter_1 = 1, counter_2 = 10; counter_1 <= counterTo_1, counter_2 >= counterTo_2; counter_1++, counter_2--){
+    txtPrintValue += " " + counter_1.toString();
+    txtPrintValue_2 += " " + counter_2.toString();
+}
+printOut(txtPrintValue + newLine);
+printOut(txtPrintValue_2);
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
@@ -19,24 +29,16 @@ example, 45. Let the computer "guess" by generating a random number. Use a "whil
 number once the "while" loop has completed. You do not need to print anything while the "while" loop is in
 progress.*/
 
-/* let number = Math.floor(Math.random()*60)+1;
-const correctNumber = 19;
-while (number == correctNumber){
-    
-    printOut("You have guessed the number!")
+let number = Math.floor(Math.random()*60)+1;
+let count = 0;
+const correctNumber = 58;
 
-} */
-let StartTime = Date.now
-const answerNumber = 45;
-let guessNumber = 0;
-while(answerNumber !== guessNumber){ //returnerer inntil answernumber er lik guessnumber//
-    guessNumber = Math.floor(Math.random()*60)+1;
-}
-printOut("Guess number = " + guessNumber.toString());
-const endTime = Date.now();
-const timeUsed = Endptime;
-PrinOtut ("An")
-
+while (number !== correctNumber){
+    number = Math.floor(Math.random()*60)+1;
+    count++;
+} 
+printOut("You have guessed the number! " + number);
+printOut("It took " + count + " guesses!")
 
 printOut(newLine);
 
@@ -44,14 +46,49 @@ printOut("--- Part 3 -----------------------------------------------------------
 /*Take the program from part 2 and expand it to guess a number between 1 and one million. Print the
 number of guesses as well as the number of milliseconds it took to guess the number. HINT: Use the
 Date.now() function to measure time*/
-printOut("Replace this with you answer!");
+
+let answerNumber = Math.floor(Math.random()*1000000)+1;
+const guessNumber = 67535;
+let startTime = Date.now();
+let count_1 = 0;
+
+while (answerNumber !== guessNumber){
+    answerNumber = Math.floor(Math.random()*1000000)+1;
+    count_1++;
+} 
+
+const endTime = Date.now() - startTime;
+printOut("Guess the number! The number is: " + guessNumber.toString());
+printOut("It took " + count_1 + " guesses")
+printOut("It took " + endTime + " ms for the computer to guess the number!" );
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /*Use a "for" loop and a "while" loop to find all prime numbers greater than 1 and less than 200.
 ○ HINT: A prime number is any natural number greater than 1 that is only divisible by itself and
 1. The number 1 is not a prime. (See Wikipedia on primes or ask your AI).*/
-printOut("Replace this with you answer!");
+
+printOut(newLine);
+// all prime numbers 1 > x > 200, prime number 
+
+ for (let num = 2; num < 200; num++) {
+    let isPrime = true;
+      
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+        isPrime = false;
+        break;
+    }
+}
+      
+    if (isPrime) {
+        printOut("Primenumber is: " + num); // Output the prime number
+    }
+ }
+
+
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
@@ -68,7 +105,18 @@ K1R5 K2R5 K3R5 K4R5 K5R5 K6R5 K7R5 K8R5 K9R5
 K1R6 K2R6 K3R6 K4R6 K5R6 K6R6 K7R6 K8R6 K9R6
 K1R7 K2R7 K3R7 K4R7 K5R7 K6R7 K7R7 K8R7 K9R7*/
 
-printOut("Replace this with you answer!");
+let row = "";  
+
+for (let R = 1; R <= 7; R++){
+    row += newLine;
+    for ( let K = 1; K <= 9; K++)
+    {
+    row += "K" + K + "R" + R + " ";
+    }
+}
+
+printOut(row + "");
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
@@ -87,7 +135,111 @@ Hint for Success: If you successfully complete the sorting challenge, you'll unl
 9 of "DAT101: Mandatory assignment 4.1" that will make it easier to solve. The learning outcomes remain
 the same, but this hint will give you a head start!*/
 
-printOut("Replace this with you answer!");
+let student_1 = (Math.round(Math.random()*236)+1)/236*100;
+let student_2 = (Math.round(Math.random()*236)+1)/236*100;
+let student_3 = (Math.round(Math.random()*236)+1)/236*100;
+let student_4 = (Math.round(Math.random()*236)+1)/236*100;
+let student_5 = (Math.round(Math.random()*236)+1)/236*100;
+
+printOut("Student 1 grade: " + student_1.toFixed(0) + ", Student 2 grade: " + student_2.toFixed(0) + ", Student 3 grade: " + student_3.toFixed(0) + ", Student 4 grade: " + student_4.toFixed(0) + ", Student 5 grade: " + student_5.toFixed(0))
+
+printOut(newLine);
+
+if (student_1 <= 40){
+    printOut("student 1: You got an F!");
+    }
+    else if (student_1 <= 52){
+        printOut("student 1: You got an E");
+    }
+    else if (student_1 <= 64){
+        printOut("student 1: You got a D");
+    }
+    else if (student_1 <= 76){
+        printOut("student 1: You got a C");
+    }
+    else if (student_1 <= 88){
+    printOut("student 1: You got a B");
+
+    }else{
+    printOut("student 1: You got an A");
+    }
+
+if (student_2 <= 40){
+    printOut("student 2: You got an F!");
+    }
+    else if (student_2 <= 52){
+        printOut("student 2: You got an E");
+    }
+    else if (student_2 <= 64){
+        printOut("student 2: You got a D");
+    }
+    else if (student_2 <= 76){
+        printOut("student 2: You got a C");
+    }
+    else if (student_2 <= 88){
+    printOut("student 2: You got a B");
+
+    }else{
+    printOut("student 2: You got an A");
+    }
+
+if (student_3 <= 40){
+    printOut("student 3: You got an F!");
+    }
+    else if (student_3 <= 52){
+        printOut("student 3: You got an E");
+    }
+    else if (student_3 <= 64){
+        printOut("student 3: You got a D");
+    }
+    else if (student_3 <= 76){
+        printOut("student 3: You got a C");
+    }
+    else if (student_3 <= 88){
+    printOut("student 3: You got a B");
+
+    }else{
+    printOut("student 3: You got an A");
+    }
+    
+if (student_4 <= 40){
+    printOut("student 4: You got an F!");
+    }
+    else if (student_4 <= 52){
+        printOut("student 4: You got an E");
+    }
+    else if (student_4 <= 64){
+        printOut("student 4: You got a D");
+    }
+    else if (student_4 <= 76){
+        printOut("student 4: You got a C");
+    }
+    else if (student_4 <= 88){
+    printOut("student 4: You got a B");
+
+    }else{
+    printOut("student 4: You got an A");
+    }
+
+if (student_5 <= 40){
+    printOut("student 5: You got an F!");
+    }
+    else if (student_5 <= 52){
+        printOut("student 5: You got an E");
+    }
+    else if (student_5 <= 64){
+        printOut("student 5: You got a D");
+    }
+    else if (student_5 <= 76){
+        printOut("student 5: You got a C");
+    }
+    else if (student_5 <= 88){
+    printOut("student 5: You got a B");
+
+    }else{
+    printOut("student 5: You got an A");
+    }
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
@@ -105,7 +257,7 @@ const d4 = Math.ceil(Math.random()*6);
 const d5 = Math.ceil(Math.random()*6);
 const d6 = Math.ceil(Math.random()*6);
 
-let diceThrow;
+let diceThrow = "";
 diceThrow += d1.toString() + ",";
 diceThrow += d2.toString() + ",";
 diceThrow += d3.toString() + ",";
@@ -113,36 +265,45 @@ diceThrow += d4.toString() + ",";
 diceThrow += d5.toString() + ",";
 diceThrow += d6.toString();
 
-printOut("Dicethrow:" + diceThrow);
-
-
-count1 = (diceThrow.match(/1/g) || "").length;
-count2 = (diceThrow.match(/2/g) || "").length;
-count3 = (diceThrow.match(/3/g) || "").length;
-count4 = (diceThrow.match(/4/g) || "").length;
-count5 = (diceThrow.match(/5/g) || "").length;
-count6 = (diceThrow.match(/6/g) || "").length;
+let count1 = (diceThrow.match(/1/g) || "").length;
+let count2 = (diceThrow.match(/2/g) || "").length;
+let count3 = (diceThrow.match(/3/g) || "").length;
+let count4 = (diceThrow.match(/4/g) || "").length;
+let count5 = (diceThrow.match(/5/g) || "").length;
+let count6 = (diceThrow.match(/6/g) || "").length;
 
 let diceCount = "";
+diceCount += count1.toString() + ",";
+diceCount += count2.toString() + ",";
+diceCount += count3.toString() + ",";
+diceCount += count4.toString() + ",";
+diceCount += count5.toString() + ",";
+diceCount += count6.toString();
 
-diceCount += count1.toString() + ",";
-diceCount += count1.toString() + ",";
-diceCount += count1.toString() + ",";
-diceCount += count1.toString() + ",";
-diceCount += count1.toString() + ",";
-diceCount += count1.toString();
-printOut("diceCount: " + diceCount);
+let counter_straight = 0;
+
+printOut("Dicethrow: " + diceThrow); //The dice numbers
+printOut("DiceCount: " + diceCount); //The amount of each dice numbers that was thrown
+
+printOut("It took " + counter_straight + " throws to get straight");
+//full straight
+
+
+
+
+
+// 3 pairs
+
+// 2 of a kind and 4 of a kind
+
+//All the same
 
 const equals1 = (diceCount.match(/1/g) || "").length;
 const equals6 = (diceCount.match(/6/g) || "").length;
-printOut("equals1: " + equals1.toString());
-printOut("equals6: " + equals6.toString());
 
+printOut("Equals 1: " + equals1.toString());
+printOut("equals 6: " + equals6.toString());
 
-printOut("Count1: " + diceoOunt.toString)
-
-
-printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
